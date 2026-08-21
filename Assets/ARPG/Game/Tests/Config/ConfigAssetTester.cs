8504e0e2eaf2232a683d760841e94a10c0251173
xChang1021x <xchang1021x@gmail.com>
2026-08-21 21:05:22 +0800

    test: add config asset integration scene

---
+++ Assets/ARPG/Game/Tests/Config/ConfigAssetTester.cs
@@ -0,0 +1,29 @@
+using ARPG.Game.Bootstrap;
+using ARPG.Game.Config;
+using UnityEngine;
+
+namespace ARPG.Game.Tests.Config
+{
+    public sealed class ConfigAssetTester
+        : MonoBehaviour
+    {
+        private void Start()
+        {
+            PlayerConfig playerConfig =
+                GameLauncher.Instance
+                    .GameContext
+                    .ConfigService
+                    .Get<PlayerConfig>();
+
+            GameLauncher.Instance
+                .GameContext
+                .LogService
+                .Info(
+                    "ConfigTest",
+                    $"PlayerConfig loaded: " +
+                    $"HP={playerConfig.MaxHealth}, " +
+                    $"Attack={playerConfig.Attack}, " +
+                    $"MoveSpeed={playerConfig.MoveSpeed}");
+        }
+    }
+}
