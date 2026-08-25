5eff67cfade40cd7ca0184a0fc6d483700f21e64
xChang1021x <xchang1021x@gmail.com>
2026-08-25 20:46:55 +0800

    feat: add ui panel lifecycle abstraction

---
+++ Assets/ARPG/Game/UI/Runtime/UILayer.cs
@@ -0,0 +1,10 @@
+namespace ARPG.Game.UI
+{
+    public enum UILayer
+    {
+        Background = 0,
+        Normal = 100,
+        Popup = 200,
+        Overlay = 300
+    }
+}
---
+++ Assets/ARPG/Game/UI/Runtime/UIPanel.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace ARPG.Game.UI
+{
+    public abstract class UIPanel : MonoBehaviour
+    {
+        public bool IsOpen { get; private set; }
+
+        internal void Open()
+        {
+            if (IsOpen)
+            {
+                return;
+            }
+
+            gameObject.SetActive(true);
+
+            IsOpen = true;
+
+            OnOpen();
+        }
+
+        internal void Close()
+        {
+            if (!IsOpen)
+            {
+                return;
+            }
+
+            OnClose();
+
+            IsOpen = false;
+
+            gameObject.SetActive(false);
+        }
+
+        protected virtual void OnOpen()
+        {
+        }
+
+        protected virtual void OnClose()
+        {
+        }
+    }
+}
